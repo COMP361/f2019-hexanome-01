4a397677415812b3eef948a3ae0994624673b3c7
laemtl <laetitia.fesselier@mail.mcgill.ca>
2019-10-28 04:07:05 -0400

    UI - Board initial sketch (map, timelines)

---
+++ Assets/Scripts/cellHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PolygonCollider2D))]
+
+public class cellHandler : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    //public Color32 oldColor;
+    private Color32 color = new Color(1f,1f,1f,0f);
+    private Color32 hoverColor = new Color(1f,0f,0f,.5f);
+    
+    void Awake() 
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.color = color;
+    }
+
+    void OnMouseEnter() 
+    {
+        sprite.color = hoverColor;
+        
+    }
+
+    void OnMouseExit() 
+    {
+        sprite.color = color;
+    }
+}
