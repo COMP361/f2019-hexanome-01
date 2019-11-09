be15441acf950c80efdb4dc10105dd963272160d
Benjamin Langlois-Therien <benjamin.langlois-therien@mail.mcgill.ca>
2019-11-08 21:54:31 -0500

    ISSUE2 Prototype move function

--- Assets/Scripts/Game/MoveButtonHandler.cs
+++ Assets/Scripts/Game/MoveButtonHandler.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveButtonHandler : MonoBehaviour
-{
+
+public class MoveButtonHandler : MonoBehaviour{
     // Start is called before the first frame update
-    public void Move()
+     public void Move()
     {
       Debug.Log ("On click");
+      var destination = Random.Range(0,3);
+      GameManager.Move(destination);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Game/Slot_follower.cs
+++ Assets/Scripts/Game/Slot_follower.cs
@@ -3,25 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slot_follower : MonoBehaviour{
-  Slot [] Slots;
-  public GameObject Hero;
+
+  public Transform[] slots;
+  private GameObject Hero;
+
+  // not sure what serializing does but the tutorial used it
+  [SerializeField]
+  private float moveSpeed = 2f;
     // Start is called before the first frame update
 
-  public float MoveSpeed;
+  // not sure what this do (it prevents the inspector to see what we serialized )but the tutorial had it
+  //[HideInspector]
+  // at the begining is initial position
 
-  int Timer;
+  public int currentPosition = 0; // Corresponds to an index in the Transform array.
+  public int destination = 0;
+  public bool canMove = false;
 
-  static Vector3 CurrentPositionHolder;
-    void Start()  {
-      Slots = GetComponentsInChildren<Slot> ();
 
-      foreach(Slot n in Slots){
-        Debug.Log (n.name);
-      }
+
+
+  private void Start()  {
+      //initialize the Player at the position he should be.
+      transform.position = slots[currentPosition].transform.position;
+
     }
 
     // Update is called once per frame
-    void Update(){
+  private void Update(){
+      if (canMove){
+        Move();
+      }
 
     }
+
+ public void Move() {
+    transform.position = Vector2.MoveTowards(transform.position, slots[destination].transform.position, moveSpeed * Time.deltaTime);
+
+    if(transform.position == slots[destination].transform.position ){
+       currentPosition = destination;
+    }
+ }
+
+
+
 }
