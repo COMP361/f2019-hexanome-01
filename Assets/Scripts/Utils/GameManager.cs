8aa09b482a1ccfa689a543999ec4d400d572d88d
Benjamin Langlois-Therien <benjamin.langlois-therien@mail.mcgill.ca>
2019-11-09 07:55:58 -0500

    Adding moving script to master

--- Assets/Scripts/Utils/GameManager.cs
+++ Assets/Scripts/Utils/GameManager.cs
@@ -1,15 +1,36 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class GameManager : MonoBehaviour{
 
 
-
+ // not sure what {get; private set} does
   public static GameManager Instance {get; private set;}
 
-  public int CurrentPlayer;
+  public static GameObject currentPlayer;
+  public static GameObject secondPlayer;
+  public static bool isOver;
+  public static int currentPlayerStart = 0;
     // Start is called before the first frame update
+
+    void Start(){
+
+      currentPlayer = GameObject.Find("Player1");
+      secondPlayer = GameObject.Find("Player2");
+    //  currentPlayer.GetComponent<Slot_follower>().canMove = true;
+
+    }
+
+   public static void Move(int destination){
+     currentPlayer.GetComponent<Slot_follower>().destination = destination;
+     currentPlayer.GetComponent<Slot_follower>().canMove = true;
+     currentPlayer.GetComponent<Slot_follower>().Move();
+     //currentPlayer.GetComponent<Slot_follower>().canMove = false;
+    //currentPlayer
+   }
+
     private void Awake()
     {
       if (Instance == null)
@@ -34,6 +55,11 @@ public class GameManager : MonoBehaviour{
     // Update is called once per frame
     void Update()
     {
-
+      if(currentPlayer.GetComponent<Slot_follower>().currentPosition == currentPlayer.GetComponent<Slot_follower>().destination){
+        currentPlayer.GetComponent<Slot_follower>().canMove = false;
+        GameObject temp = currentPlayer;
+        currentPlayer = secondPlayer;
+        secondPlayer = temp;
+      }
     }
 }
