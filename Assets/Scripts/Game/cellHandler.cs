92b296d2806c647a19a159a732ff910566626162
hamzasalah21 <hamza.salah211@gmail.com>
2019-11-10 14:20:00 -0500

    more templating.

--- Assets/Scripts/Game/cellHandler.cs
+++ Assets/Scripts/Game/cellHandler.cs
@@ -35,13 +35,30 @@ public class cellHandler : MonoBehaviour
     {
         GameObject[] accessibleCells = new GameObject[80];
         Queue<GameObject> queue = new Queue<GameObject>();
-
-
+        Dictionary<string, int> visitedCells = new Dictionary<string, int>();
+        string id = this.name;
+        
         foreach (var cell in neighbours)
         {
             queue.Enqueue(cell);
         }
 
+        while( queue.Count > 0) {
+            
+        }
+        //while (queue.Count > 0)
+        //{
+        //    var vertex = queue.Dequeue();
+
+        //    if (visited.Contains(vertex))
+        //        continue;
+
+        //    visited.Add(vertex);
+
+        //    foreach (var neighbor in graph.AdjacencyList[vertex])
+        //        if (!visited.Contains(neighbor))
+        //            queue.Enqueue(neighbor);
+        //}
 
         return accessibleCells;
     }
