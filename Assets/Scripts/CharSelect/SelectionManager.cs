898f64e7164116c02b4c9f44679bbcb1b323106c
eamonn-lye <55474132+eamonn-lye@users.noreply.github.com>
2019-11-18 23:14:51 -0500

    Transferred Enums to separate file + Updated Character Selection
    Enums.cs now contains all enums. Please put any successive enums in this file.
    
    Updated Character Selection scripts to work with HeroType enum.

--- Assets/Scripts/CharSelect/SelectionManager.cs
+++ Assets/Scripts/CharSelect/SelectionManager.cs
@@ -6,29 +6,32 @@ public class SelectionManager : MonoBehaviour {
 
     public static readonly int playerCount = 4;
     public PlayerCard[] PlayerCards;
-    int currentPlayer;
 
-    private void Awake() {
-        currentPlayer = 0;
-    }
+    private int currentPlayer = 0;
+
     void Start() {
         PlayerCards[currentPlayer].setAsCurrent();
     }
+
     public void currentPlayerLock() {
         
-        int chosenHero = PlayerCards[currentPlayer].CurrentHero;
+        HeroType chosenHero = PlayerCards[currentPlayer].CurrentHero; //chosenHero=index of the current player's hero selection at the given time
         PlayerCards[currentPlayer++].setAsLocked();
 
         if (currentPlayer < playerCount) {
-
             PlayerCards[currentPlayer].setAsCurrent();
-            for (int i = 0; i < playerCount; i++) {
-                PlayerCards[i].disableHero(chosenHero);
-                if (i > currentPlayer) PlayerCards[i].Status = "Waiting on P" + currentPlayer;
-            }
+            updatePlayerCards(chosenHero);
         } 
 
     }
+
+    private void updatePlayerCards(HeroType heroLocked) {
+        for (int i = 0; i < playerCount; i++) {
+            PlayerCards[i].disableHero(heroLocked); //disable the ability to select the hero for all other players
+            if (i > currentPlayer) PlayerCards[i].Status = "Waiting on P" + currentPlayer; //all cards of players AFTER the current is updated to "waiting on <player>"
+        }
+    }
+
     public void startGame() {
 
     }
