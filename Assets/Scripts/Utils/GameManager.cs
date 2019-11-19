69faf4756b00433c75f6daad312d4ee43947c585
eamonn-lye <55474132+eamonn-lye@users.noreply.github.com>
2019-11-18 23:27:57 -0500

    Removed Action Enum definition from GameManager
    the changes were undone before I did the previous merge below for some reason.

--- Assets/Scripts/Utils/GameManager.cs
+++ Assets/Scripts/Utils/GameManager.cs
@@ -2,11 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Action {
-    Think,
-    Move,
-    Fight
-};
 
 public class GameManager : MonoBehaviour{
 
