e44e888119a7ae25d976f8c8326449c6067d871b
hamzasalah21 <hamza.salah211@gmail.com>
2020-01-24 15:50:31 -0500

    Room succesfully connecting and created.

---
+++ Assets/photon_lobby/Scripts/RoomLayoutGroup.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomLayoutGroup : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _roomListingPrefab;
+    private GameObject RoomListingPrefab
+    {
+        get { return _roomListingPrefab; }
+    }
+
+    private List<RoomListing> _roomListingButtons = new List<RoomListing>();
+    private List<RoomListing> RoomListingButtons
+    {
+        get { return _roomListingButtons; }
+    }
+
+    private void OnReceiveRoomListUpdate()
+    {
+        RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+
+        foreach (RoomInfo room in rooms)
+        {
+            RoomReceived(room);
+        }
+
+        RemoveOldRooms();
+    }
+    
+    // Checks if the rooms have been received.
+    private void RoomReceived(RoomInfo room)
+    {
+        // This is going to look through all the roomListingButtons and find the right one if it equals room
+        int index = RoomListingButtons.FindIndex(x => x.RoomName == room.Name);
+
+        if (index == -1)
+        {
+            if(room.IsVisible && room.playerCount < room.MaxPlayers)
+            {
+                GameObject roomListingObj = Instantiate(RoomListingPrefab);
+                roomListingObj.transform.SetParent(transform, false);
+
+                RoomListing roomListing = roomListingObj.GetComponent<RoomListing>();
+                RoomListingButtons.Add(roomListing);
+
+                index = (RoomListingButtons.Count - 1);
+            }
+        }
+
+        if (index != -1)
+        {
+            RoomListing roomListing = RoomListingButtons[index]; //find the room
+            roomListing.SetRoomNameText(room.Name); // Update the name
+            roomListing.Updated = true;
+        }
+    }
+
+    private void RemoveOldRooms()
+    {
+        List<RoomListing> removeRooms = new List<RoomListing>();
+
+        foreach(RoomListing roomListing in RoomListingButtons)
+        {
+            if (roomListing.Updated)
+                removeRooms.Add(roomListing);
+            else
+                roomListing.Updated = false;
+        }
+
+        foreach (RoomListing roomListing in removeRooms)
+        {
+            GameObject roomListingObj = roomListing.gameObject;
+            RoomListingButtons.Remove(roomListing);
+            Destroy(roomListingObj);
+        }
+    }
+}
