719550c29b40664143286c72f9d23a94b3abc3e8
Richard Lei <46201873+richcs@users.noreply.github.com>
2020-02-05 20:56:29 -0500

    Update CharacterSelectionStartGame.cs

--- Assets/Scripts/CharSelect/CharacterSelectionStartGame.cs
+++ Assets/Scripts/CharSelect/CharacterSelectionStartGame.cs
@@ -8,7 +8,7 @@ public class CharacterSelectionStartGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,6 +19,6 @@ public class CharacterSelectionStartGame : MonoBehaviour
 
     public void StartGameClick()
     {
-        SceneManager.LoadScene("board");
+        SceneManager.LoadScene("Game");
     }
 }
