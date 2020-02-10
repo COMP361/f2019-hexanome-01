16d3ac0a01788176df31f6942b39a902a9e9559b
Benjamin <blangloistherien@gmail.com>
2020-02-09 19:37:29 -0500

    Inventory_UI

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private bool inventoryEnable;
+    public GameObject inventory;
+
+    private int allSpots;
+    private int enabledSpots;
+    private GameObject[] spot;
+
+    public GameObject spotHolder ;
+
+    void start(){
+      allSpots = 3;
+      spot = new GameObject[allSpots];
+      for(int i = 0; i < allSpots; i++){
+        spot[i] = spotHolder.transform.GetChild(i).gameObject;
+      }
+    }
+
+    void update(){
+       if(Input.GetKeyDown(KeyCode.I)){
+         inventoryEnable = !inventoryEnable;
+       }
+
+       if(inventoryEnable){
+         inventory.SetActive(true);
+       }else{
+         inventory.SetActive(false);
+       }
+
+    }
+}
--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -5,8 +5,63 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class EventManager {
+    //GameManager gm;
+
+    // this class helps to register the events that are happening and trigger all the functions that are linked to this event.
+
+
+  //  public delegate void PlayerUpdateHandler(Hero hero);
+  //  public static event PlayerUpdateHandler PlayerUpdate;
+
+  //  public delegate void ActionUpdateHandler(Action action);
+  //  public static event ActionUpdateHandler ActionUpdate;
+
+  //  public delegate void MoveSelectHandler();
+//    public static event MoveSelectHandler MoveSelect;
+
+//    public delegate void SkipSelectHandler();
+//    public static event SkipSelectHandler SkipSelect;
+
+  //  public delegate void FightSelectHandler();
+  //  public static event FightSelectHandler FightSelect;
+
     public delegate void CellClickHandler(int cellID);
     public static event CellClickHandler CellClick;
+
+  //  public delegate void MoveConfirmHandler();
+  //  public static event MoveConfirmHandler MoveConfirm;
+
+//    public delegate void MoveCancelHandler();
+//    public static event MoveCancelHandler MoveCancel;
+
+//    public delegate void StartTurnHandler();
+//    public static event StartTurnHandler StartTurn;
+
+//    public delegate void EndTurnHandler();
+  //  public static event EndTurnHandler EndTurn;
+
+    //
+    public delegate void CellHoverInHandler(int cellID);
+    public static event CellHoverInHandler CellHoverIn;
+
+    public delegate void CellHoverOutHandler();
+    public static event CellHoverOutHandler CellHoverOut;
+
+    /*void Start() {
+        gm = GameManager.instance;
+    }*/
+
+    /*void Update() {
+        // Check if click is not over a UI element
+        if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if(hit.collider != null) {
+                hit.collider.gameObject.GetComponent<IClickHandler>().OnClick();
+            }
+        }
+    }*/
+
     public static void TriggerCellClick(int cellID) {
         if (CellClick != null) {
             CellClick(cellID);
@@ -29,6 +84,14 @@ public class EventManager {
         }
     }
 
+  /*  public static void TriggerMoveSelect() {
+        EventManager.TriggerActionUpdate(Action.Move);
+
+        if (MoveSelect != null) {
+            MoveSelect();
+        }
+    }
+*/
     public delegate void FightSelectHandler();
     public static event FightSelectHandler FightSelect;
     public static void TriggerFightSelect() {
@@ -113,6 +176,25 @@ public class EventManager {
         }
     }
 
+/*
+    public static void TriggerEndTurn() {
+        if (EndTurn != null) {
+            EndTurn();
+        }
+    } */
+
+    // if some functions are waiting for the event to happen, call the event.
+    public static void TriggerCellHoverIn(int cellID) {
+        if (CellHoverIn != null) {
+            CellHoverIn(cellID);
+        }
+    }
+
+    public static void TriggerCellHoverOut() {
+        if (CellHoverOut != null) {
+            CellHoverOut();
+        }
+      }
     public delegate void MoveCompleteHandler(Movable movable);
     public static event MoveCompleteHandler MoveComplete;
     public static void TriggerMoveComplete(Movable movable) {
@@ -149,7 +231,7 @@ public class EventManager {
     public static event EndDayHandler EndDay;
     public static void TriggerEndDaySelect() {
           if (EndDay != null) {
-            EndDay();   
+            EndDay();
           }
     }
 }
