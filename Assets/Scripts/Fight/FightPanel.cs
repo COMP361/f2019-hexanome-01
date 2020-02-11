a96a7088480b2de7325df5f8ebfbfe9be61a3c7b
hamzasalah21 <hamza.salah211@gmail.com>
2020-02-11 10:30:11 -0500

    restarting fight.

---
+++ Assets/Scripts/Fight/FightPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FightPanel : MonoBehaviour
+{
+    private Text playerName;
+    private Text monsterName;
+    private Text playerStrength;
+    private Text playerWP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
