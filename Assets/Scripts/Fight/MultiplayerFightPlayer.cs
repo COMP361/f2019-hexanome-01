af607b395cc6f417906f91e1926722b308c4d98a
hamzasalah21 <hamza.salah211@gmail.com>
2020-03-28 20:42:23 -0400

    created the choice to go multiplayer or single player.

--- Assets/Scripts/Fight/MultiplayerFightPlayer.cs
+++ Assets/Scripts/Fight/MultiplayerFightPlayer.cs
@@ -41,27 +41,108 @@ public class MultiplayerFightPlayer : MonoBehaviour
     }
 
     private HeroFighter mage = new HeroFighter("Mage");
+    public Button mage_button;
     private HeroFighter archer = new HeroFighter("Archer");
+    public Button archer_button;
     private HeroFighter warrior = new HeroFighter("Warrior");
+    public Button warrior_button;
     private HeroFighter dwarf = new HeroFighter("Dwarf");
+    public Button dwarf_button;
 
     public void activateMage()
     {
         mage.isPresent = !mage.isPresent;
+
+        ColorBlock myColorBlock = new ColorBlock();
+        if (mage.isPresent)
+        {
+            myColorBlock.normalColor = new Color(121, 7, 242, 1);
+            mage_button.colors = myColorBlock;
+        }
+        else
+        {
+            myColorBlock.normalColor = Color.white;
+            mage_button.colors = myColorBlock;
+        }
     }
 
     public void activateArcher()
     {
         archer.isPresent = !archer.isPresent;
+
+        ColorBlock myColorBlock = new ColorBlock();
+        if (archer.isPresent)
+        {
+            myColorBlock.normalColor = Color.green;
+            archer_button.colors = myColorBlock;
+        }
+        else
+        {
+            myColorBlock.normalColor = Color.white;
+            archer_button.colors = myColorBlock;
+        }
     }
 
     public void activateWarrior()
     {
         warrior.isPresent = !warrior.isPresent;
+
+        ColorBlock myColorBlock = new ColorBlock();
+        if (warrior.isPresent)
+        {
+            myColorBlock.normalColor = Color.blue;
+            warrior_button.colors = myColorBlock;
+        }
+        else
+        {
+            myColorBlock.normalColor = Color.white;
+            warrior_button.colors = myColorBlock;
+        }
     }
 
     public void activateDwarf()
     {
         dwarf.isPresent = !dwarf.isPresent;
+
+        ColorBlock myColorBlock = new ColorBlock();
+        if (dwarf.isPresent)
+        {
+            myColorBlock.normalColor = Color.yellow;
+            dwarf_button.colors = myColorBlock;
+        }
+        else
+        {
+            myColorBlock.normalColor = Color.white;
+            dwarf_button.colors = myColorBlock;
+        }
+    }
+
+    private void InTheCellHeroes()
+    {
+        Cell currentCell = GameManager.instance.CurrentPlayer.Cell;
+        List<string> hero_types = new List<string>();
+        List<Button> buttons = new List<Button> { warrior_button, mage_button, archer_button, dwarf_button };
+
+        foreach (Hero h in currentCell.Inventory.Heroes)
+        {
+            hero_types.Add(h.Type.ToLower());
+        }
+
+        if (hero_types.Contains("archer"))
+        {
+            archer_button.enabled = true;
+        }
+        if (hero_types.Contains("warrior"))
+        {
+            warrior_button.enabled = true;
+        }
+        if (hero_types.Contains("mage"))
+        {
+            mage_button.enabled = true;
+        }
+        if (hero_types.Contains("dwarf"))
+        {
+            dwarf_button.enabled = true;
+        }
     }
 }
