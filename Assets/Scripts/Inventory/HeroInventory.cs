07f000c5d8888420f1a4d8f68fcf4713df0a9aae
Benjamin <blangloistherien@gmail.com>
2020-04-08 08:38:56 -0400

    heroInventory cleanup

--- Assets/Scripts/Inventory/HeroInventory.cs
+++ Assets/Scripts/Inventory/HeroInventory.cs
@@ -1,4 +1,3 @@
-using System;
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +7,6 @@ using Photon.Realtime;
 using  System.Collections.Specialized;
 public class HeroInventory
 {
-  //  public List<SmallToken> smallTokens { get; private set; }
-  //  public List<GoldCoin> golds { get; private set; }
-  //   public List<Token> AllTokens { get; private set; }
-
     public BigToken bigToken { get; private set; }
     public Helm helm { get; private set; }
 
@@ -25,11 +20,9 @@ public class HeroInventory
     public OrderedDictionary smallTokens { get; private set; }
 
     void OnEnable() {
-  //  EventManager.InventoryUICellEnter += updateUI;
     }
 
     void OnDisable() {
-    //  EventManager.InventoryUICellEnter -= updateUI;
     }
 
     public HeroInventory(string parentHero){
@@ -51,7 +44,6 @@ public class HeroInventory
 
     public void AddGold(GoldCoin gold){
       int viewID = gold.GetComponent<PhotonView>().ViewID;
-    //  GameManager.instance.AddGoldHeroRPC(viewID, parentHero);
       GameManager.instance.photonView.RPC("AddGoldHeroRPC", RpcTarget.AllViaServer, new object[] {viewID, parentHero});
 
     }
@@ -73,13 +65,11 @@ public class HeroInventory
       if(numOfGold >= amtToRemove){
         for(int i = 0; i< amtToRemove; i++){
           int viewID = ((GoldCoin)golds[i]).GetComponent<PhotonView>().ViewID;
-        //  GameManager.instance.RemoveGoldHeroRPC(viewID, parentHero);
           GameManager.instance.photonView.RPC("RemoveGoldHeroRPC", RpcTarget.AllViaServer, new object[] {viewID, parentHero});
 
         }
       }
       else{
-        // error not enough gold
       }
     }
 
@@ -104,7 +94,6 @@ public class HeroInventory
       }
       else{
         int viewID = smallToken.GetComponent<PhotonView>().ViewID;
-      //  GameManager.instance.AddSmallTokenRPC(viewID, parentHero);
         GameManager.instance.photonView.RPC("AddSmallTokenRPC", RpcTarget.AllViaServer, new object[] {viewID, parentHero});
 
 
@@ -127,7 +116,6 @@ public class HeroInventory
     public void RemoveSmallToken(SmallToken smallToken){
       if(smallTokens.Contains(convertToKey(smallToken.GetComponent<PhotonView>().ViewID))){
       int viewID = smallToken.GetComponent<PhotonView>().ViewID;
-    //  GameManager.instance.RemoveSmallTokenRPC(viewID, parentHero);
       GameManager.instance.photonView.RPC("RemoveSmallTokenRPC", RpcTarget.AllViaServer, new object[] {viewID, parentHero});
 
       }
@@ -153,13 +141,11 @@ public class HeroInventory
     public bool AddBigToken(BigToken item){
       if(bigToken == null){
         int viewID = item.GetComponent<PhotonView>().ViewID;
-      //  GameManager.instance.AddBigTokenRPC(viewID, parentHero);
         GameManager.instance.photonView.RPC("AddBigTokenRPC", RpcTarget.AllViaServer, new object[] {viewID, parentHero});
 
         return true;
       }
       else{
-        //Error already has a big token
         EventManager.TriggerError(1);
         return false;
       }
@@ -167,6 +153,7 @@ public class HeroInventory
 
     public void AddBigToken2(BigToken item){
       string id = convertToKey(item.GetComponent<PhotonView>().ViewID);
+      AllTokens.Add(id,(BigToken)item);
       bigToken = item;
       if(GameManager.instance.MainHero.TokenName.Equals(parentHero)){
         EventManager.TriggerInventoryUIHeroUpdate(this);
@@ -179,7 +166,6 @@ public class HeroInventory
     public void RemoveBigToken(BigToken item){
       if(bigToken.GetComponent<PhotonView>().ViewID == item.GetComponent<PhotonView>().ViewID){
         int viewID = item.GetComponent<PhotonView>().ViewID;
-      //  GameManager.instance.RemoveBigTokenRPC(viewID, parentHero);
         GameManager.instance.photonView.RPC("RemoveBigTokenRPC", RpcTarget.AllViaServer, new object[] {viewID, parentHero});
 
       }
@@ -191,6 +177,7 @@ public class HeroInventory
     public void RemoveBigToken2(int viewID){
       if(bigToken.GetComponent<PhotonView>().ViewID == viewID){
         bigToken = null;
+        AllTokens.Remove(convertToKey(viewID));
       }
       if(GameManager.instance.MainHero.TokenName.Equals(parentHero)){
         EventManager.TriggerInventoryUIHeroUpdate(this);
@@ -203,7 +190,6 @@ public class HeroInventory
     public bool AddHelm(Helm item){
       if(helm == null){
         int viewID = item.GetComponent<PhotonView>().ViewID;
-      //  GameManager.instance.AddHelmRPC(viewID, parentHero);
         GameManager.instance.photonView.RPC("AddHelmRPC", RpcTarget.AllViaServer, new object[] {viewID, parentHero});
 
         return true;
@@ -218,6 +204,7 @@ public class HeroInventory
     public void AddHelm2(Helm item){
       string id = convertToKey(item.GetComponent<PhotonView>().ViewID);
       helm = item;
+      AllTokens.Add(id,(Helm)item);
       if(GameManager.instance.MainHero.TokenName.Equals(parentHero)){
         EventManager.TriggerInventoryUIHeroUpdate(this);
       }
@@ -229,7 +216,6 @@ public class HeroInventory
     public void RemoveHelm(Helm item){
       if(helm.GetComponent<PhotonView>().ViewID == item.GetComponent<PhotonView>().ViewID){
         int viewID = item.GetComponent<PhotonView>().ViewID;
-        //GameManager.instance.RemoveHelmRPC(viewID, parentHero);
         GameManager.instance.photonView.RPC("RemoveHelmRPC", RpcTarget.AllViaServer, new object[] {viewID, parentHero});
 
       }
@@ -241,6 +227,7 @@ public class HeroInventory
     public void RemoveHelm2(int viewID){
       if(helm.GetComponent<PhotonView>().ViewID == viewID){
         helm = null;
+        AllTokens.Remove(convertToKey(viewID));
       }
       if(GameManager.instance.MainHero.TokenName.Equals(parentHero)){
         EventManager.TriggerInventoryUIHeroUpdate(this);
@@ -251,7 +238,6 @@ public class HeroInventory
     }
 
 
-  //  public void updateUI(CellInventory inventory, int index){}
 
     public string convertToKey(int a){
       string toReturn = "" + a;
@@ -285,102 +271,3 @@ public class HeroInventory
   #endregion
 
 }
-
-/*
-    public bool AddSmallToken(Token token){
-
-      if(smallTokens.Count >= spaceSmall){
-        Debug.Log("Not enough room ");
-        return false;
-      }
-       smallTokens.Add((SmallToken)token);
-       EventManager.TriggerInventoryUIHeroUpdate(this);
-       return true;
-    }
-
-    public bool AddBigToken(Token token){
-      if(bigToken != null){
-        Debug.Log("Not enough room ");
-        return false;
-      }
-       bigToken = (BigToken)token;
-       EventManager.TriggerInventoryUIHeroUpdate(this);
-       return true;
-    }
-
-    public bool AddHelm(Token token){
-      if(helm != null){
-        Debug.Log("Not enough room ");
-        return false;
-      }
-       helm = (Helm)token;
-       EventManager.TriggerInventoryUIHeroUpdate(this);
-       return true;
-    }
-
-    // maybe have a void return type
-    /*
-    public bool AddGold(Token token){
-      golds.Add((GoldCoin)token);
-      AllTokens.Add(token);
-      numOfGold++;
-      if(GameManager.instance.MainHero.heroInventory == this){
-        EventManager.TriggerInventoryUIHeroUpdate(this);
-      }
-      return true;
-    } */
-
-
-
-    /*
-        public void RemoveToken(Token token){
-            Type listType;
-            listType = smallTokens.GetListType();
-            if (listType.IsCompatibleWith(token.GetType())) {
-                smallTokens.Remove((SmallToken)token);
-                return;
-              }
-            listType = BigToken.GetType();
-            if (listType.IsCompatibleWith(token.GetType())) {
-              if (bigToken == token){
-                bigToken = null;
-              }
-              return;
-            }
-
-            listType = helm.GetType();
-            if (listType.IsCompatibleWith(token.GetType())) {
-              if (helm == token){
-                helm = null;
-              }
-              return;
-            }
-
-            listType = golds.GetListType();
-            if (listType.IsCompatibleWith(token.GetType())) {
-              golds.Remove((GoldCoin)token);
-              return;
-            }
-            AllTokens.Remove(token);
-            EventManager.TriggerInventoryUIHeroUpdate(this);
-        }
-
-    /*
-        public void RemoveGold(int amtToRemove)
-        {
-            if (golds.Count >= amtToRemove)
-            {
-                numOfGold-= amtToRemove;
-                while(amtToRemove != 0)
-                {
-                    amtToRemove--;
-                    if(amtToRemove < golds.Count) {
-                      AllTokens.Remove(golds[amtToRemove]);
-                      golds.RemoveAt(amtToRemove);
-                    }
-                }
-
-            }
-            EventManager.TriggerInventoryUIHeroUpdate(this);
-        }
-    */
