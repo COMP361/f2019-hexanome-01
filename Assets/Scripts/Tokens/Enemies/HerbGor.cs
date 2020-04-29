81c659342ebe245f2ce4ce7a3dccbe49583d0414
Laetitia Fesselier <laetitia.fesselier@mail.mcgill.ca>
2020-04-28 22:34:39 -0400

    Fixes

--- Assets/Scripts/Tokens/Enemies/HerbGor.cs
+++ Assets/Scripts/Tokens/Enemies/HerbGor.cs
@@ -25,7 +25,7 @@ public class HerbGor : Gor
     
     protected void OnDestroy() {
         if(quitting) return;
-        Herb.Factory(Cell.Index);
+        if(GameManager.instance.CurrentPlayer == GameManager.instance.MainHero) Herb.Factory(Cell.Index);
         base.OnDestroy();
     }
 }
